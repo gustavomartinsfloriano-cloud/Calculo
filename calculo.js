368e893828d311bf973fcb16b29d9debf312f3d4
Gustavo Floriano <gustavo.martins.floriano@escola.pr.gov.br>
2026-07-30 11:38:09 -0300

    Add files via upload

---
+++ calculo.js
@@ -0,0 +1,9 @@
+alert("hello word");
+let numero1; //variavel que vai receber numero1
+let numero2; //variavel que vai receber numero2
+let resultado; //variavel que vai mostra o resultado
+
+numer1 = 3; //variavel numero1 recebe o primeiro valor
+numer2 = 5; //variuavel numer2 recebe o seguyndo valolr
+resultado = numero1 + numero2; //variavel resultado recebe 
+alert(resultado); //variavel resultado e mostrada na tela
